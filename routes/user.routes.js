ad1df583bf7664f57e9c53efc159441e6b87bb7c
Abhijit Mondal <abhijitamp378@gmail.com>
2025-12-31 16:11:19 +0530

    error

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
 
 router.get('/register', (req, res) => {
     res.render('register'); 
 });
 
-router.post('/register', (req, res) => {
-    console.log(req.body);
-    res.send('User registered successfully');
+router.post('/register', 
+    body('username').isLength({ min: 5 }).withMessage('Username must be at least 5 characters long'),
+    body('email').isEmail().withMessage('Please enter a valid email address'),
+    body('password').isLength({ min: 8 }).withMessage('Password must be at least 8 characters long'),   
+    
+    (req, res) => {
+
+    const errors = validationResult(req);
+    console.log(errors);
+    
+    res.send(errors)
 }); 
 
 module.exports = router;
