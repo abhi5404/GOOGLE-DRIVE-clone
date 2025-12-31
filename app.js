6a20bfe7dfc748598aa5d3bf6f99d9bcb79752a1
Abhijit Mondal <abhijitamp378@gmail.com>
2025-12-31 15:29:00 +0530

    routes created for user routes

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const app = express();
+const userRoutes = require('./routes/user.routes');
 
 app.set('view engine', 'ejs');
+app.use('/users', userRoutes);
 
-app.get('/', (req, res) => {
-  res.render('index');
-});
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
