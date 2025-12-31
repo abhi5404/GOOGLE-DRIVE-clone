2f6293d53739df313d2d4808b03a3f5346a2cea7
Abhijit Mondal <abhijitamp378@gmail.com>
2025-12-31 17:52:21 +0530

    home.ejs done

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
+dotenv.config();
 const connectDB = require('./config/db');
 connectDB();
 const userRoutes = require('./routes/user.routes');
-dotenv.config();
 const cookieParser = require('cookie-parser');
 const app = express();
+const indexRoutes = require('./routes/index.routes');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/', indexRoutes);
 
 app.set('view engine', 'ejs');
 app.use(cookieParser());
