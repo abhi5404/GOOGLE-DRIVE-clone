f606c4299b2fc0d481c14e477505e01d7d136732
Abhijit Mondal <abhijitamp378@gmail.com>
2025-12-31 15:33:18 +0530

    tailwind

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 
-router.get('/test', (req, res) => {
-    res.send('User route is working!');
+router.get('/register', (req, res) => {
+    res.render('register'); 
 });
 
 module.exports = router;
