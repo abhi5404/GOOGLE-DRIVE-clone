0d09f5641bf75d4a7ed560af8c8789e9d55d78a3
Abhijit Mondal <abhijitamp378@gmail.com>
2025-12-31 17:15:38 +0530

    jwt  installed

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { body, validationResult } = require('express-validator');
 const User = require('../models/user.model');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 router.get('/register', (req, res) => {
   res.render('register');
@@ -43,4 +44,32 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
+router.post('/login',
+   body('username').isLength({ min: 5 }).withMessage('Username must be at least 5 characters long'),
+   body('password').isLength({ min: 8 }).withMessage('Password must be at least 8 characters long'),
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array(),
+                message: 'Validation failed'
+            });
+        }
+        const { username, password } = req.body;
+        const user= await userModel.findOne({
+            username: username
+        });
+        if (!user) {
+            return res.status(400).json({
+                message: 'Invalid username or password'
+            });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({
+                message: 'Invalid username or password'
+            });
+        }
+    }); 
 module.exports = router;
